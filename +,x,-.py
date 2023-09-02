8feacae785ba959fc1c4b85e402de5f1e9c32867
Victor <kva0705@mail.ru>
2023-09-02 14:52:28 +0300

    function sum

--- +,x,-.py
+++ +,x,-.py
@@ -2,5 +2,8 @@ class plus:
     def __init__(self,x,y):
         self.addendum_1= x
         self.addendum_2 = y
+    def sum(self):
+        s = self.addendum_1 + self.addendum_2
+        print(s)
 
 

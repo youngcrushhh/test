2789af0e97c9d7eb82f52adb8e4f3fbcd9a47f3d
Victor <kva0705@mail.ru>
2023-09-02 16:08:45 +0300

    print rational number

--- reverse_and_reduction.py
+++ reverse_and_reduction.py
@@ -3,5 +3,9 @@ class reduction:
     def __init__(self,x,y):
         self.num= x
         self.denom = y
+    def print(self):
+        print(self.num)
+        print('-')
+        print(self.denom)
 
 

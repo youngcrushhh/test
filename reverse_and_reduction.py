d9c5bdcf10cfeb5fb614985d4f4f75633ffd4de9
Victor <kva0705@mail.ru>
2023-09-10 17:31:03 +0300

    all is done

--- reverse_and_reduction.py
+++ reverse_and_reduction.py
@@ -1,26 +1,49 @@
 import math
-class reduction:
+class rational:
     def __init__(self,x,y):
-        self.num= x
+        self.num = x
         self.denom = y
-    def print(self):
-        print(self.num)
-        print('-')
-        print(self.denom)
+    def __str__(self):
+        return str(self.num) + '\n' + '-' + '\n' + str(self.denom)
     def redc(self):
-        global k
         k = math.gcd(self.num, self.denom)
-        print(k)
-    def devide(self):
-        n = self.num // k
-        d = self.denom // k
-        print(n)
-        print("-")
-        print(d)
+        self.num= self.num // k
+        self.denom= self.denom // k
+    def __mul__(self,rational_2):
+        a = self.num * rational_2.num
+        b = self.denom * rational_2.denom
+        c = rational(a,b)
+        c.redc()
+        return c
+    def __truediv__(self,rational_2: int):
+        self.num *= rational_2.denom
+        self.denom *= rational_2.num
+    def __add__(self,rational_2: int):
+        a = self.num * rational_2.denom + self.denom * rational_2.num
+        b = self.denom * rational_2.denom
+        c = rational(a,b)
+        c.redc()
+        return c
+    def __sub__(self,rational_2: int):
+        a = self.num * rational_2.denom - self.denom * rational_2.num
+        b = self.denom * rational_2.denom
+        c = rational(a, b)
+        c.redc()
+        return c
 
 
-reduction_1 = reduction(15,75)
-reduction_1.print()
-reduction_1.redc()
-reduction_1.devide()
+
+
+
+
+
+
+rational_1 = rational(15,75)
+rational_2 = rational(1,5)
+rational_3 = rational_1 * rational_2
+rational_4 = rational_1 + rational_2
+rational_5 = rational_1 - rational_2
+print(rational_5)
+
+
 
